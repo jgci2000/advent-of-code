48e5357bd2180246424b82bc876eb81bbd18cabc
João Inácio <jgci2000@hotmail.com>
2021-12-07 11:14:38 +0100

    solution to both problems

--- day4/bingo.py
+++ day4/bingo.py
@@ -1,6 +1,8 @@
 
+filename = "input.txt"
+
 n_boards = 0
-with open("input.txt", "r") as file:
+with open(filename, "r") as file:
     file.readline()
     for line in file:
         n_boards += 1
@@ -17,7 +19,7 @@ def score(board, marked, draw):
                 s += board[n_row][n_col]
     return s * draw
 
-with open("input.txt", "r") as file:
+with open(filename, "r") as file:
     sequence = [int(c) for c in file.readline().strip().split(",")]
     for i in range(n_boards):
         file.readline()
@@ -32,25 +34,31 @@ for i, draw in enumerate(sequence):
         for n_line, line in enumerate(boards[n][0]):
             if draw in line:
                 boards[n][1][n_line][line.index(draw)] = 1
-    
-    for n in boards.keys():
+
+    keys = list(boards.keys())
+    for n in keys:
         sum_col = list()
         for j in range(5):
             sum_col.append(0)
-
+    
+        broken = False
         for n_row in range(5):
             for j, c in enumerate(boards[n][1][n_row]):
                 sum_col[j] += c
                 if sum_col[j] == 5:
                     print(f"board {n} wins with score {score(boards[n][0], boards[n][1], draw)} here")
-                    print(draw)
-                    exit()
+                    boards.pop(n)
+                    broken = True
+                    break
+            if broken:
+                break
 
             if sum(boards[n][1][n_row]) == 5:
                 print(f"board {n} wins with score {score(boards[n][0], boards[n][1], draw)}")
-                print(draw)
-                exit()
-
+                boards.pop(n)
+                break
+    if len(boards) == 0:
+        break
 
 
 
